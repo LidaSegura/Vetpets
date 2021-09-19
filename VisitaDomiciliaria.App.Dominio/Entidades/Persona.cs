7b6e530d3e61ae52577c7ad273053a8df8bbbde5
LidaSegura <asistente@molecait.com>
2021-09-19 18:08:30 -0500

    Version1

---
+++ VisitaDomiciliaria.App.Dominio/Entidades/Persona.cs
@@ -0,0 +1,11 @@
+namespace VisitaDomiciliaria.App.Dominio
+{
+    public class Persona
+    {
+        public string Id {get;set;}
+        public string Nombre {get;set;}
+        public string Apellido {get;set;}
+        public string Direccion {get;set;}
+        public string Telefono {get;set;}
+    }
+}
