7deee336d3067beca28fa726a2371c73a3dab60b
LidaSegura <asistente@molecait.com>
2021-09-19 19:16:36 -0500

    Version2

---
+++ VisitaDomiciliaria.App.Dominio/Entidades/Propietario.cs
@@ -0,0 +1,8 @@
+namespace VisitaDomiciliaria.App.Dominio
+{
+    public class Propietario : Persona
+    {
+        public string Correo {get;set;}
+        public string IdMascota {get;set;}
+    }
+}
